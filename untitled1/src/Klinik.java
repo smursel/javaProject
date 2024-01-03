ea8cade029e634d60f0f1658535a70ae7e8454af
HasanArifDogan <114689311+HasanArifDogan@users.noreply.github.com>
2024-01-03 12:48:14 +0300

    Add files via upload

---
+++ untitled1/src/Klinik.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Klinik{
+    public String klinikAdi ="";
+    public ArrayList<String> branslar = new ArrayList<String>();
+
+    public Klinik(String klinikAdi, ArrayList<String> branslar){
+        this.klinikAdi = klinikAdi;
+        this.branslar = branslar;
+    }
+}
