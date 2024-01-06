b234197bd750697df07adc71496ca8f3ec53c562
smursel <115039439+smursel@users.noreply.github.com>
2024-01-06 17:50:43 +0300

    Delete untitled1 directory

--- untitled1/src/Hekim.java
+++
@@ -1,7 +0,0 @@
-public class Hekim extends User {
-    private Klinik klinik;//enum
-    private Hastane hastane;
-    private String brans;
-
-    private static final int auth = 1;
-}
--- untitled1/src/Yonetim.java
+++
@@ -1,8 +0,0 @@
-public interface Yonetim {
-
-    Object KullaniciBul(String id);
-    void Yarat(Object kullanici);
-    void KullaniciGuncelle(String mevcutKullaniciId ,Object KullaniciYeniHali);
-    void KullaniciSil(String id);
-
-}
